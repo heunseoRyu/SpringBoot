84530d6e35f9489257d30cfb35f1a44256d7f0cb
heunseoRyu <120763372+heunseoRyu@users.noreply.github.com>
2023-06-01 23:14:19 +0900

    spring-mvc
    Model View Controller

--- hello-spring/src/main/java/hellospring/hellospring/Controller/HelloController.java
+++ hello-spring/src/main/java/hellospring/hellospring/Controller/HelloController.java
@@ -3,6 +3,7 @@ package hellospring.hellospring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "야발");
         return "hello";
+    }
+
+    @GetMapping("hell-mvc")//HelloController가 아니라 hell-mvc라는 주소로 name인수에 값을 받는다.
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";//template으로 감.
     }
 }
