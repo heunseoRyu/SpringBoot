4629b059f6c81696e2cb97e9841936e57cb28a3d
heunseoRyu <120763372+heunseoRyu@users.noreply.github.com>
2023-06-02 08:34:18 +0900

    hello-api
    use @ResponseBody
    1. string
    2. json

--- hello-spring/src/main/java/hellospring/hellospring/Controller/HelloController.java
+++ hello-spring/src/main/java/hellospring/hellospring/Controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,4 +22,36 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";//template으로 감.
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody //바디부분에 이 내용을 직접 넣어 주겠다는 의미(뭔 개소리냐) 소스 보기 보면 이해가 바로됨.
+    //진짜 저 값이 바로 내려감.
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name; //"hello spring" 이거를 그대로 내려줌.
+    }
+
+    // json 방식 : key랑 value로 정리된 방식 (소스코드 보기 하면 이해됨.)
+    //그냥 요즘에 객체는 json으로 반환해준다.
+    @GetMapping("hello-api")
+    @ResponseBody //몸에 바로 내려라
+    public Hello HelloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();//hello객체 만들기
+        hello.setName(name);//setName메소드 활용해 hello.name=name값 받은거
+        return hello; //hello 객체 반환 -> 내려간 값 : {"name":"spring"}
+    } //클래스 Hello의 객체를 반환하는 메소드
+    //HelloController 클래스안에 Hello 클래스 정의 -> static 쓰면 가능
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+
+    }
+
 }
